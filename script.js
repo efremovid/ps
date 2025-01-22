777a7dfd61c0d981901b121343f1c619231b602d
efremovid <i.efremov.psk@yandex.com>
2025-01-22 19:04:42 +0300

    попробуем

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 onload = window['dialog-id'].showModal();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollButton = document.getElementById('scrollbt');
+    const targetElement = document.getElementById('scrolltg');
+
+    // Обработчик клика на кнопку
+    scrollButton.addEventListener('click', () => {
+        // Прокручиваем страницу до указанного элемента с плавной анимацией
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
