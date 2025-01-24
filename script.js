9b2e9dff5425a42982c57b4b1348cc8c53aa8c18
efremovid <i.efremov.psk@yandex.com>
2025-01-24 18:54:25 +0300

    попробуем

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 onload = window['dialog-id'].showModal();
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const scrollButton = document.getElementById('scrollbt');
-    const targetElement = document.getElementById('scrolltg');
-
-    // Обработчик клика на кнопку
-    scrollButton.addEventListener('click', () => {
-        // Прокручиваем страницу до указанного элемента с плавной анимацией
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollButton = document.querySelector('.scroll-button');
+    const targetElement = document.querySelector('.target-block');
+    scrollButton.addEventListener('click',
+        function () {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollButton = document.querySelector('.scroll-actions');
+    const targetElement = document.querySelector('.actions');
+    scrollButton.addEventListener('click',
+        function () {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollButton = document.querySelector('.scroll-contacts');
+    const targetElement = document.querySelector('.footer');
+    scrollButton.addEventListener('click',
+        function () {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollButton = document.querySelector('.about_scroll');
+    const targetElement = document.querySelector('.target-block');
+    scrollButton.addEventListener('click',
+        function () {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollButton = document.querySelector('.contacts_scroll');
+    const targetElement = document.querySelector('.footer');
+    scrollButton.addEventListener('click',
+        function () {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+});
+
